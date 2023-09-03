a134d857f6c776b449874f3a9ac32b7e7e0c95f1
Vivek Vaish <vaishvivek634@gmail.com>
2023-09-03 16:48:59 +0530

    styling css for mobile view

--- src/App.js
+++ src/App.js
@@ -10,32 +10,40 @@ function App() {
 
   return (
     <>
-      <h1>Border Radius Generator</h1>
+      <h2>Border Radius Generator</h2>
       <div className="app">
         <div className="left">
           <div style={{ borderRadius: radius }} className="square"></div>
-          <div className="label">{radius}</div>
+          <div className="label">border-radius: {radius}</div>
         </div>
         <div className="right">
           <div>
             <label htmlFor="topleft">Top Left:</label>
-            <input type="number" id='topleft' className="number" value={topLeft} onChange={e => setTopLeft(e.target.value)} />
-            <input type="range" className="range" min='0' max='200' value={topLeft} onChange={e => setTopLeft(e.target.value)} />
+            <div>
+              <input type="number" id='topleft' className="number" value={topLeft} onChange={e => setTopLeft(e.target.value)} />
+              <input type="range" className="range" min='0' max='200' value={topLeft} onChange={e => setTopLeft(e.target.value)} />
+            </div>
           </div>
           <div>
             <label htmlFor="topright">Top Right:</label>
-            <input type="number" id='topright' className="number" value={topRight} onChange={e => setTopRight(e.target.value)} />
-            <input type="range" className="range" min='0' max='200' value={topRight} onChange={e => setTopRight(e.target.value)} />
+            <div>
+              <input type="number" id='topright' className="number" value={topRight} onChange={e => setTopRight(e.target.value)} />
+              <input type="range" className="range" min='0' max='200' value={topRight} onChange={e => setTopRight(e.target.value)} />
+            </div>
           </div>
           <div>
             <label htmlFor="bottomright">Bottom Right:</label>
-            <input type="number" id='bottomright' className="number" value={bottomRight} onChange={e => setBottomRight(e.target.value)} />
-            <input type="range" className="range" min='0' max='200' value={bottomRight} onChange={e => setBottomRight(e.target.value)} />
+            <div>
+              <input type="number" id='bottomright' className="number" value={bottomRight} onChange={e => setBottomRight(e.target.value)} />
+              <input type="range" className="range" min='0' max='200' value={bottomRight} onChange={e => setBottomRight(e.target.value)} />
+            </div>
           </div>
           <div>
             <label htmlFor="bottomleft">Bottom Left:</label>
-            <input type="number" id='bottomleft' className="number" value={bottomLeft} onChange={e => setBottomLeft(e.target.value)} />
-            <input type="range" className="range" min='0' max='200' value={bottomLeft} onChange={e => setBottomLeft(e.target.value)} />
+            <div>
+              <input type="number" id='bottomleft' className="number" value={bottomLeft} onChange={e => setBottomLeft(e.target.value)} />
+              <input type="range" className="range" min='0' max='200' value={bottomLeft} onChange={e => setBottomLeft(e.target.value)} />
+            </div>
           </div>
         </div>
       </div>
